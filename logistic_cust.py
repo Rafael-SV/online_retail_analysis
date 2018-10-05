191fe7b997a2c309bd3f148c0ec687c7c6236797
L_Lizard <spazzmo1EE6@Rafaels-MacBook-Pro.local>
2018-10-04 18:03:22 -0700

    take a peak at value counts

--- logistic_cust.py
+++ logistic_cust.py
@@ -7,13 +7,15 @@ import datetime as dt
 warnings.filterwarnings("ignore")
 
 
-data = pd.read_csv("data.csv", encoding="ISO-8859-1", dtype={'CustomerID': str, 'InvoiceNO': str})
-data = data.dropna()
-print(data.shape)
-print(list(data.columns))
-data.head()
-print(data.shape)
-data = data.dropna()
+data_raw = pd.read_csv("data.csv", encoding="ISO-8859-1", dtype={'CustomerID': str, 'InvoiceNo': str})
+print(data_raw.shape)
+print(list(data_raw.columns))
+data_raw.head()
+print(data_raw.shape)
+data = data_raw.dropna()
 print(data.shape)
 data = data[data['Quantity']>0]
 print(data.shape)
+print('duplicate entries: {}'.format(data.duplicated().sum()))
+
+pd.DataFrame([{'products': len(data['StockCode'].value_counts()), 'transactions': len(data['InvoiceNo'].value_counts()), 'customers': len(data['CustomerID'].value_counts()), }], columns = ['products', 'transactions', 'customers'])
