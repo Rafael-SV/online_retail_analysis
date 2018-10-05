ed1e5f508645b520820a1bb2eec87692c8b04aeb
L_Lizard <spazzmo1EE6@Rafaels-MacBook-Pro.local>
2018-10-04 18:07:27 -0700

    added comments about data examination and cleaning steps

--- logistic_cust.py
+++ logistic_cust.py
@@ -6,16 +6,24 @@ import datetime as dt
 
 warnings.filterwarnings("ignore")
 
-
+# read file
 data_raw = pd.read_csv("data.csv", encoding="ISO-8859-1", dtype={'CustomerID': str, 'InvoiceNo': str})
+# take a peak at structure of raw input
 print(data_raw.shape)
 print(list(data_raw.columns))
 data_raw.head()
 print(data_raw.shape)
+
+# time to start cleaning the data
 data = data_raw.dropna()
 print(data.shape)
+
+# remove returned items
 data = data[data['Quantity']>0]
+# check for how many were removed
 print(data.shape)
+#check for dupes
 print('duplicate entries: {}'.format(data.duplicated().sum()))
 
+# take a look at value counts
 pd.DataFrame([{'products': len(data['StockCode'].value_counts()), 'transactions': len(data['InvoiceNo'].value_counts()), 'customers': len(data['CustomerID'].value_counts()), }], columns = ['products', 'transactions', 'customers'])
